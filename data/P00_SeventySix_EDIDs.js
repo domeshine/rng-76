99f894cffac8fa8d5aa2480a8999a9164270123e
domeshine <27443012+domeshine@users.noreply.github.com>
2026-06-10 02:16:28 -0400

    Testing: P00

---
+++ data/P00_SeventySix_EDIDs.js
@@ -0,0 +1,10 @@
+edids = {
+"008A52C8": "LVLIATX_Resources_LL_Collectron_Peppino_LegendaryGear_Rank2",
+"008A52C6": "LVLIATX_Resources_LL_Collectron_Peppino_LegendaryGear_Rank3",
+"008A52C7": "LVLIATX_Resources_Collectron_Peppino",
+"008A52C4": "LVLIATX_Resources_LL_Collectron_Peppino_LegendaryGear_Rank1",
+"008A52C5": "LVLIATX_Resources_Collectron_LegendaryGear_VeryRare",
+"008A6FC3": "LVLIATX_Resources_LLS_Collectron_Peppino_Base",
+"00857249": "ENTMATX_ENTM_CAMP_Utility_Collectron_Peppino Peppino the Clown Collectron Station",
+"0086DBF1": "PKINATX_Collectron_packin_Peppino Peppino Collectron Station",
+}
---
+++ data/P00_SeventySix_LVLIs.js
@@ -0,0 +1,116 @@
+leveledLists = {
+"008A6FC3": {
+  "LVLF": 2,
+  "Entries": [
+    {
+      "Object": "00059B1C"
+    },
+    {
+      "Object": "0031F1B3"
+    },
+    {
+      "Object": "000330FC"
+    },
+    {
+      "Object": "0058F9E1"
+    },
+    {
+      "Object": "00060ED8"
+    },
+    {
+      "Object": "000DF266"
+    }
+  ]
+},
+"008A52C6": {
+  "LVLF": 3,
+  "Entries": [
+    {
+      "Object": "00417C44"
+    },
+    {
+      "Object": "00417C48"
+    },
+    {
+      "Object": "00417C41"
+    }
+  ]
+},
+"008A52C5": {
+  "LVLF": 64,
+  "Entries": [
+    {
+      "Object": "008A52C4"
+    },
+    {
+      "Object": "008A52C8",
+      "Conditions": [
+          {
+            "Operator": -96,
+            "Value": 20.0,
+            "Function": 4173,
+            "FunctionName": "GetRandomPercent",
+            "Param1Ref": "00000000",
+            "Param1Value": 0.0,
+            "Param2Ref": "00000000",
+            "Param2Value": 0.0,
+            "RunOn": "Subject"
+          }
+      ]
+    },
+    {
+      "Object": "008A52C6",
+      "Conditions": [
+          {
+            "Operator": -96,
+            "Value": 10.0,
+            "Function": 4173,
+            "FunctionName": "GetRandomPercent",
+            "Param1Ref": "00000000",
+            "Param1Value": 0.0,
+            "Param2Ref": "00000000",
+            "Param2Value": 0.0,
+            "RunOn": "Subject"
+          }
+      ]
+    }
+  ]
+},
+"008A52C4": {
+  "LVLF": 3,
+  "Entries": [
+    {
+      "Object": "00417C42"
+    },
+    {
+      "Object": "00417C46"
+    },
+    {
+      "Object": "00417C4B"
+    }
+  ]
+},
+"008A52C8": {
+  "LVLF": 3,
+  "Entries": [
+    {
+      "Object": "00417C43"
+    },
+    {
+      "Object": "00417C47"
+    },
+    {
+      "Object": "00417C40"
+    }
+  ]
+},
+"008A52C7": {
+  "LVMV": 1.000000,
+  "LVLF": 4,
+  "Entries": [
+    {
+      "Object": "008A6FC3"
+    }
+  ]
+}
+}
